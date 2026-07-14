6ce88b3bfcd549d2d3e036ba5f44a614af463886
mtdnot <mtdnot1129@gmail.com>
2026-07-14 18:36:09 +0900

    Initialize portfolio repository

---
+++ u-aizu_portfolio/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  devToolbar: {
+    enabled: false
+  },
+  build: {
+    format: 'directory'
+  }
+});
